ddfe4039afe664445543d32939a0601916f65afb
vjabuilds <jovan.vjestica@outlook.com>
2022-10-28 15:45:49 +0200

    Updated tests to check for hydra

--- tests/setup/pyspark_test.py
+++ tests/setup/pyspark_test.py
@@ -1,4 +1,20 @@
+import pytest
 import pyspark
+from hydra import initialize, compose
 
-def test_pyspark_install():
-    sc = pyspark.SparkContext('local[*]', "PySpark install test")
+@pytest.fixture
+def hydra_cfg():
+    with initialize(version_base=None, config_path="../../datawaves-pipeline-runner/configs"):
+        cfg = compose(config_name="config")
+        return cfg
+
+def test_hydra_default(hydra_cfg):
+    assert hydra_cfg == {
+        "spark" : {
+            "head" : "local[*]"
+        },
+        "name": "datawaves-pipeline-runner"
+    }
+
+def test_pyspark_install(hydra_cfg):
+    sc = pyspark.SparkContext(hydra_cfg.spark.head, "PySpark install test")
