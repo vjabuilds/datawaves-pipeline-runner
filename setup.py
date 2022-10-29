c25ac19202ca6967bb102a55acbdaf16b3e2f9ec
vjabuilds <jovan.vjestica@outlook.com>
2022-10-29 20:03:16 +0200

    Changed name to use underscores

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'datawaves-pipeline-runner',
+    name = 'datawaves_pipeline_runner',
     version = '0.0.1',
     packages = find_packages()
 )
