e70bd4d53f65e1ba013895c83644c3f7e01e1814
vjabuilds <jovan.vjestica@outlook.com>
2022-11-14 11:19:31 +0100

    Removed spark context from AbstractOperator

--- datawaves_pipeline_runner/operators/core/abstract_operator.py
+++ datawaves_pipeline_runner/operators/core/abstract_operator.py
@@ -1,8 +1,7 @@
 from abc import ABC, abstractmethod
-from pyspark import SparkContext
 
 
 class AbstractOperator(ABC):
     @abstractmethod
-    def operate(self, spark: SparkContext):
+    def operate(self):
         return
