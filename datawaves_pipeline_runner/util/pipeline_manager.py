91bdfb855c50c12787e4b98c8f47b05d0cef1d7d
vjabuilds <jovan.vjestica@outlook.com>
2022-11-25 11:49:00 +0100

    Added pipeline serialization

--- datawaves_pipeline_runner/util/pipeline_manager.py
+++ datawaves_pipeline_runner/util/pipeline_manager.py
@@ -1,11 +1,13 @@
 import hydra
 from hydra.utils import instantiate
+import os
 from omegaconf import OmegaConf
 
 from .spark_session_utils import get_spark
+from ..operators.core import AbstractOperator
 
 
-class PipelineManager:
+class PipelineManager            :
     def __init__(self):
         self._pipeline = None
         OmegaConf.register_resolver("spark_resolver", get_spark)
@@ -19,3 +21,13 @@ class PipelineManager:
 
     def load_pipeline_from_conf(self, conf: OmegaConf):
         self._pipeline = instantiate(conf)
+
+    def set_pipeline(self, pipeline: AbstractOperator):
+        self._pipeline = pipeline
+
+    def save_pipeline(self, path: str, flat: bool = True):
+        conf = self._pipeline.to_dictionary()
+        if flat:
+            OmegaConf.save(conf, path)
+        else:
+            raise NotImplemented
